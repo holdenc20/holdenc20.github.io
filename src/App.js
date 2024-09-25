50fb3a36b4ed444d900778a226b1d66e1bc55957
holdenc20 <kcholden@me.com>
2024-09-25 19:16:38 -0400

    Formatting nav

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       {/* Header Section */}
       <header className="App-header">
-        <nav>
+        <nav className="navbar">
           <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
