5eca773b4c18d1771feffa189be101e1c5d466d3
holdenc20 <kcholden@me.com>
2024-09-25 19:09:08 -0400

    Adding nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Casey's Portfolio</h1>
+      {/* Header Section */}
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+
+
+      {/* Home Section */}
+      <section id="home">
+        <h1>Home</h1>
+        <p>Welcome to my website!</p>
+      </section>
+  
+        {/* About Section */}
+      <section id="about">
+        <h1>About</h1>
+        <p>My name is Casey.</p>
+      </section>
     </div>
   );
 }
